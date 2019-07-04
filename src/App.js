823cf4756caf1310b92c875a1f698df4dfcce420
Riley Mathews <rileymathews80@gmail.com>
2019-07-04 16:41:46 -0500

    added simple auth form

--- src/App.js
+++ src/App.js
@@ -1,12 +1,42 @@
 import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <React.Fragment>
-      <h1>This is the app</h1>
-    </React.Fragment>
-  );
+class App extends React.Component {
+  state = {
+    bool: true,
+    username: "",
+    password: "",
+  }
+
+  inputUpdate = evt => {
+    const state = { ...this.state }
+    state[evt.target.id] = evt.target.value
+    this.setState(state)
+  }
+
+  signIn = () => {
+    console.log("Signing in")
+  }
+
+  signUp = () => {
+    console.log("Signing up")
+  }
+
+  render() {
+    return(
+      <React.Fragment>
+        <h1>This is the app</h1>
+        <form>
+          <label for="username">Username</label>
+          <input type="text" id="username" placeholder="username" value={this.state.username} onChange={this.inputUpdate}></input>
+          <label for="password">Password</label>
+          <input type="password" id="password" placeholder="username" value={this.state.password} onChange={this.inputUpdate}></input>
+        </form>
+        <button onClick={this.signIn}>Sign In</button>
+        <button onClick={this.signUp}>Sign Up</button>
+      </React.Fragment>
+    )
+  }
 }
 
 export default App;
